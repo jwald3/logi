bd1613de558ea1b1d85d9a4d7e7dc90b9da81471
Jordan Riesel <jwald3@brockport.edu>
2021-12-09 00:56:52 -0500

    refactored to utilize connector classes

--- src/main/java/logi/util/Connector.java
+++ src/main/java/logi/util/Connector.java
@@ -3,13 +3,13 @@ package logi.util;
 import java.sql.Connection;
 
 public interface Connector<T> {
-    public Connection getConnection();
+    Connection getConnection();
 
-    public void executeQuery(String query);
+    void executeQuery(String query);
 
-    public void insertRecord(T object);
+    void insertRecord(T object);
 
-    public void updateRecord(T object, String primaryKey);
+    void updateRecord(T object, String primaryKey);
 
     public void deleteRecord(T object, String primaryKey);
 }
